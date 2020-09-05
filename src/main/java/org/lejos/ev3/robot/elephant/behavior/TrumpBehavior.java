c061c84412f9953c24b5cbfe65632bbb6d23c7f2
mfbieber <mfbieber@gmail.com>
2020-09-06 01:39:38 +0200

    removed unneeded logic of duration in TrumpBehavior, since the KeepControlDecorator is already taking care of making the action run only once.

--- src/main/java/org/lejos/ev3/robot/elephant/behavior/TrumpBehavior.java
+++ src/main/java/org/lejos/ev3/robot/elephant/behavior/TrumpBehavior.java
@@ -5,15 +5,11 @@ import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.robotics.subsumption.Behavior;
 import org.lejos.ev3.robot.elephant.sensor.TouchSensor;
 
-import java.time.Duration;
-
 public class TrumpBehavior implements Behavior {
 
 	private final EV3LargeRegulatedMotor motor;
 	private final TouchSensor touchsensor;
 
-	private long lastControl;
-	
 	public TrumpBehavior(EV3LargeRegulatedMotor motor, TouchSensor touchsensor) {
 		this.motor = motor;
 		this.touchsensor = touchsensor;
@@ -21,15 +17,7 @@ public class TrumpBehavior implements Behavior {
 	
 	public boolean takeControl() {
 		int button = Button.readButtons();
-		if (button != Button.ID_UP) {
-			return false;
-		}
-		Duration passedTimeAfterLastControl = Duration.ofNanos(System.nanoTime() - lastControl);
-		if (passedTimeAfterLastControl.minus(Duration.ofMillis(500)).isNegative()) {
-			return false;
-		}
-		lastControl = System.nanoTime();
-		return true;
+		return button == Button.ID_UP;
 	}
 
 	public void suppress() {
